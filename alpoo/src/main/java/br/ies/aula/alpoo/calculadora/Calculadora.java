49dc5efe848a88fe068ae0cb568903318d7bdfb1
cisraelBreno <cisraelbreno@gmail.com>
2020-08-17 20:35:58 -0300

    Calculadora do Israel v1

---
+++ alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
@@ -0,0 +1,9 @@
+package br.ies.aula.alpoo.calculadora;
+
+public class Calculadora {
+
+	public Object retornaResultado() {
+		return 3;
+	}
+
+}
---
+++ alpoo/src/test/java/alpoo/TesteCalculadora.java
@@ -0,0 +1,16 @@
+package alpoo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.ies.aula.alpoo.calculadora.Calculadora;
+
+public class TesteCalculadora {
+
+	@Test
+	public void somaComResultado3() {
+		Calculadora calculadora = new Calculadora();
+		assertEquals(3, calculadora.retornaResultado());
+	}
+}
