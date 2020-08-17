5fe47674043e60cd89ace75a83565c659db90c9c
joaop <joaop@DESKTOP-9E9BF0C>
2020-08-17 20:35:56 -0300

    Calculadora Joao

---
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/TesteOla.java
@@ -0,0 +1,13 @@
+package test.br.ies.aula.alpoo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TesteOla {
+
+	
+	@Test
+	public void ola() {
+		assertEquals (3,2+2/2);
+}
