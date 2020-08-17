0b4ec6892a572840a248891455e648b74962c82b
rafaeln2 <61211848+rafaeln2@users.noreply.github.com>
2020-08-17 20:35:03 -0300

    Calculadora Rafael Nunes V1

--- alpoo/src/test/java/test/br/ies/aula/alpoo/TesteCalculadora.java
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/TesteCalculadora.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import br.ies.aula.alpoo.calculadora.Calculadora;
 
 public class TesteCalculadora {
-	
+	 
 	@Test
 	public void somaComResultado3() {		
 		Calculadora calculadora = new Calculadora();
