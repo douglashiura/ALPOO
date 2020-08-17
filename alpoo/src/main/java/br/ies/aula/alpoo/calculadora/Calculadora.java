16fb5685b7db988eae3754eb9a32580013b30ade
joaoheing <joaoheing@hotmail.com>
2020-08-17 20:34:34 -0300

    Calculadora do Douglas v1

---
+++ alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
@@ -0,0 +1,9 @@
+package br.ies.aula.alpoo.calculadora;
+
+public class Calculadora {
+
+	public Object obterResultado() {
+		return 3;
+	}
+
+}
---
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/TesteCalculadora.java
@@ -0,0 +1,16 @@
+package test.br.ies.aula.alpoo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.ies.aula.alpoo.calculadora.Calculadora;
+
+public class TesteCalculadora{
+	
+	@Test
+	public void resultado3() {
+		Calculadora calculadora = new Calculadora();
+		assertEquals(3,calculadora.obterResultado());
+	}
+}
