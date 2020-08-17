65a774d8db9cba802b0847abeab8da3091d57dda
home <home@DESKTOP-1HMKA9G>
2020-08-17 20:45:23 -0300

    Calculadora Mizael V1

---
+++ alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
@@ -0,0 +1,11 @@
+package br.ies.aula.alpoo.calculadora;
+
+public class Calculadora {
+
+	public Object obterResultado() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+
+}
---
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/TesteCalculadora.java
@@ -0,0 +1,18 @@
+package test.br.ies.aula.alpoo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.ies.aula.alpoo.calculadora.Calculadora;
+
+public class TesteCalculadora {
+	
+	@Test
+	public void somaComResultado3() {
+		Calculadora calculadora = new Calculadora();
+		
+		assertEquals(3,calculadora.obterResultado());
+	}
+
+}
