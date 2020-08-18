0c918462b60fba815a88276988e75f08ac413de9
guisalmeida <guilherme.dasilva@linx3.com>
2020-08-17 21:47:53 -0300

    feat: add div method to calculator

--- alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
+++ alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
@@ -15,5 +15,13 @@ public class Calculadora {
 	public void multi(Integer num1, Integer num2) {
 		resultado = num1 * num2;		
 	}
+	
+	public void div(Integer num1, Integer num2) {
+		if (num1 == 0 || num2 == 0 ) {
+			resultado = 0;
+			return;
+		}
+		resultado = num1 / num2;		
+	}
 
 }
--- alpoo/src/test/java/test/br/ies/aula/alpoo/CalculatorTest.java
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/CalculatorTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 import br.ies.aula.alpoo.calculadora.Calculadora;
 
-public class TestCalculator {
+public class CalculatorTest {
 
 	private Calculadora calculadora;
 
@@ -32,6 +32,18 @@ public class TestCalculator {
 	public void multResult10() throws Exception {
 		calculadora.multi(2,5);
 		assertEquals(Integer.valueOf(10),calculadora.obterResultado());
+	}
+	
+	@Test
+	public void divResult5() throws Exception {
+		calculadora.div(10,2);
+		assertEquals(Integer.valueOf(5),calculadora.obterResultado());
+	}
+	
+	@Test
+	public void divResult0() throws Exception {
+		calculadora.div(10,0);
+		assertEquals(Integer.valueOf(0),calculadora.obterResultado());
 	}
 	
 }
