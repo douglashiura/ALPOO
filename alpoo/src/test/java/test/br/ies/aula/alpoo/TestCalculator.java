2436f7f77706af9f5fabed8fc882212cd7cc492e
guisalmeida <guilherme.dasilva@linx3.com>
2020-08-17 21:37:41 -0300

    refactor: adjust before method

--- alpoo/src/test/java/test/br/ies/aula/alpoo/TestCalculator.java
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/TestCalculator.java
@@ -8,9 +8,9 @@ import org.junit.Test;
 import br.ies.aula.alpoo.calculadora.Calculadora;
 
 public class TestCalculator {
-	
+
 	private Calculadora calculadora;
-	
+
 	@Before
 	public void setUp() {
 		calculadora = new Calculadora();
@@ -18,14 +18,12 @@ public class TestCalculator {
 	
 	@Test
 	public void sumResult3() {
-		Calculadora calculadora = new Calculadora();
 		calculadora.soma(2,1);
 		assertEquals(Integer.valueOf(3),calculadora.obterResultado());
 	}
 	
 	@Test
 	public void sumResult2() throws Exception {
-		Calculadora calculadora = new Calculadora();
 		calculadora.soma(1,1);
 		assertEquals(Integer.valueOf(2),calculadora.obterResultado());
 	}
