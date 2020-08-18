85c5ef3f2d26e20dfc2c6e106aa4d624806b1f7a
bernardo <bernardo@pop-os.localdomain>
2020-08-17 21:25:45 -0300

    funcao de soma da calculadora implementada

--- alpoo/src/main/java/br/edu/ies/alpoo/Calculator.java
+++ alpoo/src/main/java/br/edu/ies/alpoo/Calculator.java
@@ -1,7 +1,13 @@
 package br.edu.ies.alpoo;
 
 public class Calculator {
+	Object resultado;
+	
 	public Object getResult() {
-		return 3;
+		return resultado;
+	}
+	
+	public void soma(Integer x,Integer y) {
+		this.resultado = x + y; 
 	}
 }
--- alpoo/src/test/java/test/br/edu/ies/alpoo/TestOla.java
+++ alpoo/src/test/java/test/br/edu/ies/alpoo/TestOla.java
@@ -1,19 +1,31 @@
 package test.br.edu.ies.alpoo;
 
 import org.junit.Test;
+import org.junit.Before;
 
 import br.edu.ies.alpoo.Calculator;
 
 import static org.junit.Assert.assertEquals;
 
+
 public class TestOla {
+	private Calculator calculator;
+	
+	@Before
+	public void setUp() {		
+		calculator = new Calculator();
+	}
 	
 	@Test
 	public void addWithResult3() {
-		Calculator calculator = new Calculator();
-		Object expectedValue = 3;
-		
-		assertEquals(expectedValue, calculator.getResult());
+		calculator.soma(1,2);
+		assertEquals(Integer.valueOf(3), calculator.getResult());
+	}
+	
+	@Test
+	public void somaComResultado2() throws Exception {
+		calculator.soma(1,1);
+		assertEquals(Integer.valueOf(2), calculator.getResult());
 	}
 	
 
