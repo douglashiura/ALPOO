7987b0a5d676941b26515037e1f34d7c3b64a490
home <home@DESKTOP-1HMKA9G>
2020-08-17 22:06:14 -0300

    Calculadora Mizael V4

--- alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
+++ alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
@@ -2,22 +2,29 @@ package br.ies.aula.alpoo.calculadora;
 
 public class Calculadora {
 	
-	private Integer resultado;
+	private Float resultado;
 
-	public Object obterResultado() {
+	public Float obterResultado() {
 		return resultado;
 	}
 
 	public void soma(Integer parcelaA, Integer parcelaB) {
-		resultado = parcelaA + parcelaB;
+		resultado = ((float)parcelaA + parcelaB);
+	}
+	
+	public void subtrair(Integer parcelaA, Integer parcelaB) {
+		resultado = ((float)parcelaA - parcelaB);
 	}
 	
 	public void multiplica(Integer parcelaA, Integer parcelaB) {
-		resultado = parcelaA * parcelaB;
+		resultado = ((float)parcelaA * parcelaB);
 	}
 	
 	public void divide(Integer parcelaA, Integer parcelaB) {
-		resultado = parcelaA / parcelaB;
+		if(parcelaB.equals(0)) {
+			throw new ArithmeticException();
+		}
+		resultado = ((float)parcelaA / parcelaB);
 	}
 
 
--- alpoo/src/test/java/test/br/ies/aula/alpoo/TesteCalculadora.java
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/TesteCalculadora.java
@@ -19,32 +19,42 @@ public class TesteCalculadora {
 	@Test
 	public void somaComResultado3() {
 		calculadora.soma(2, 1);
-		assertEquals(Integer.valueOf(3),calculadora.obterResultado());
+		assertEquals(Float.valueOf(3),calculadora.obterResultado());
 	}
 	@Test
 	public void SomaComResultado2() throws Exception {
 		calculadora.soma(1, 1);
-		assertEquals(Integer.valueOf(2),calculadora.obterResultado());
+		assertEquals(Float.valueOf(2),calculadora.obterResultado());
+	}
+	@Test
+	public void SubtrairComResultado2() throws Exception {
+		calculadora.subtrair(6, 4);
+		assertEquals(Float.valueOf(2),calculadora.obterResultado());
+	}
+	@Test
+	public void SubtrairComResultado5() throws Exception {
+		calculadora.subtrair(9, 4);
+		assertEquals(Float.valueOf(5),calculadora.obterResultado());
 	}
 	@Test
 	public void MultiComResultado10() throws Exception {
 		calculadora.multiplica(5, 2);
-		assertEquals(Integer.valueOf(10),calculadora.obterResultado());
+		assertEquals(Float.valueOf(10),calculadora.obterResultado());
 	}
 	@Test
 	public void MultiComResultado36() throws Exception {
 		calculadora.multiplica(6, 6);
-		assertEquals(Integer.valueOf(36),calculadora.obterResultado());
+		assertEquals(Float.valueOf(36),calculadora.obterResultado());
 	}
 	@Test
 	public void DivideComResultado3() throws Exception {
 		calculadora.divide(36, 12);
-		assertEquals(Integer.valueOf(3),calculadora.obterResultado());
+		assertEquals(Float.valueOf(3),calculadora.obterResultado());
 	}
 	@Test
 	public void DivideComResultado7() throws Exception {
 		calculadora.divide(35, 5);
-		assertEquals(Integer.valueOf(7),calculadora.obterResultado());
+		assertEquals(Float.valueOf(7),calculadora.obterResultado());
 	}
 
 }
