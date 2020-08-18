d38966d9dff883d9a28c2490a9e6c3333c5abd25
guisalmeida <guilherme.dasilva@linx3.com>
2020-08-17 22:10:42 -0300

    feat: add more tests

--- alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
+++ alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
@@ -2,26 +2,25 @@ package br.ies.aula.alpoo.calculadora;
 
 public class Calculadora {
 	
-	private Integer resultado;
+	private Float resultado;
 
-	public Integer obterResultado() {
+	public Float obterResultado() {
 		return resultado;
 	}
 
 	public void soma(Integer num1, Integer num2) {
-		resultado = num1 + num2;		
+		resultado = (float) (num1 + num2);		
 	}
 	
 	public void multi(Integer num1, Integer num2) {
-		resultado = num1 * num2;		
+		resultado = (float) (num1 * num2);		
 	}
 	
-	public void div(Integer num1, Integer num2) {
-		if (num1 == 0 || num2 == 0 ) {
-			resultado = 0;
-			return;
+	public void div(Integer num1, Integer num2) throws Exception {
+		if (num2.equals(0)) {
+			throw new ArithmeticException("Não é possivel dividir por zero.");
 		}
-		resultado = num1 / num2;		
+		resultado = ((float)num1/num2);	
 	}
 
 }
--- alpoo/src/test/java/test/br/ies/aula/alpoo/CalculatorTest.java
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/CalculatorTest.java
@@ -19,31 +19,36 @@ public class CalculatorTest {
 	@Test
 	public void sumResult3() {
 		calculadora.soma(2,1);
-		assertEquals(Integer.valueOf(3),calculadora.obterResultado());
+		assertEquals(Float.valueOf(3),calculadora.obterResultado());
 	}
 	
 	@Test
 	public void sumResult2() throws Exception {
 		calculadora.soma(1,1);
-		assertEquals(Integer.valueOf(2),calculadora.obterResultado());
+		assertEquals(Float.valueOf(2),calculadora.obterResultado());
 	}
 	
 	@Test
 	public void multResult10() throws Exception {
 		calculadora.multi(2,5);
-		assertEquals(Integer.valueOf(10),calculadora.obterResultado());
+		assertEquals(Float.valueOf(10),calculadora.obterResultado());
 	}
 	
 	@Test
 	public void divResult5() throws Exception {
 		calculadora.div(10,2);
-		assertEquals(Integer.valueOf(5),calculadora.obterResultado());
+		assertEquals(Float.valueOf(5),calculadora.obterResultado());
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void divResult0() throws Exception {
+		calculadora.div(1,0);
 	}
 	
 	@Test
-	public void divResult0() throws Exception {
-		calculadora.div(10,0);
-		assertEquals(Integer.valueOf(0),calculadora.obterResultado());
+	public void divFloat() throws Exception {
+		calculadora.div(5,2);
+		assertEquals(Float.valueOf(2.5f),calculadora.obterResultado());
 	}
 	
 }
