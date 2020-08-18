356f0ffd682c8087d8c5b2de99b43db3753a12a9
Vitor Costa <VitorCosta98@github.com>
2020-08-17 21:28:42 -0300

    created new test with another value returned

--- alpoo/src/test/java/test/br/ies/aula/alpoo/CalculatorTest.java
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/CalculatorTest.java
@@ -1,16 +1,30 @@
 package test.br.ies.aula.alpoo;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import test.br.ies.aula.alpoo.calculator.Calculator;
 
 public class CalculatorTest {
+	private Calculator calculator;
+
+	@Before
+	public void setUp() {
+		calculator = new Calculator();
+	}
 	
 	@Test
 	public void addWithResult3() {
-		Calculator calculator = new Calculator();
-		Object expectedValue = 3;
+		calculator.add(2, 1);
 		
-		assertEquals(expectedValue, calculator.getResult());
+		assertEquals(3, calculator.getResult());
+	}
+	
+	@Test
+	public void addWithResult2() {
+		calculator.add(1, 1);
+		
+		assertEquals(2, calculator.getResult());
 	}
 }
