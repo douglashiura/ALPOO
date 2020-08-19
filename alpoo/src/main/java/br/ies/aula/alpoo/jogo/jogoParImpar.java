5922f7fb47c909ef24295a7a6e25f655b2a4b36e
ferna <ferna@192.168.15.12>
2020-08-19 20:41:35 -0300

    Aula 2 Calculadora e jogo de Impar/Par.  Fernando

---
+++ alpoo/src/main/java/br/ies/aula/alpoo/jogo/jogoParImpar.java
@@ -0,0 +1,46 @@
+package br.ies.aula.alpoo.jogo;
+
+import java.util.Scanner;
+
+public class jogoParImpar {
+	
+	
+
+	public static void main(String[] args) {
+		jogarComScan();
+	}
+	
+	public static void jogarComScan() {
+		int n1;
+		int n2;
+		Scanner scan = new Scanner(System.in);
+		String num1 = scan.nextLine();
+		String num2 = scan.nextLine();	
+		n1 = Integer.parseInt(num1);
+		n2 = Integer.parseInt(num2);
+		
+		if (n1 % 2 == 0) {
+			System.out.println("O numero "+n1+" é par.");
+			}
+		else {System.out.println("O numero "+n1+" é ímpar.");}
+		
+		if (n2 % 2 == 0) {
+			System.out.println("O numero "+n2+" é par.");
+			}
+		else {System.out.println("O numero "+n2+" é ímpar.");}		
+	}
+
+	public static void jogarSemScan(int n1, int n2) {	
+		if (n1 % 2 == 0) {
+			System.out.println("O numero "+n1+" é par.");
+			}
+		else {System.out.println("O numero "+n1+" é ímpar.");}
+		
+		if (n2 % 2 == 0) {
+			System.out.println("O numero "+n2+" é par.");
+			}
+		else {System.out.println("O numero "+n2+" é ímpar.");}		
+	}
+	
+	
+}
---
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/testeJogoParImpar.java
@@ -0,0 +1,23 @@
+package test.br.ies.aula.alpoo;
+import br.ies.aula.alpoo.jogo.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class testeJogoParImpar {
+
+	
+	
+	
+	
+	@Test
+	public void jogarParImpar() throws Exception {		
+		jogoParImpar  jogo =  new jogoParImpar();
+		int n1 = 2;
+		int n2 = 3;
+		jogo.jogarSemScan(n1, n2);
+}
+	
+	
+}
