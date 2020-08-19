03575614b99e94f1309f92a05c6fa77008afbaca
Victor Roberto <victoracc2001@hotmail.com>
2020-08-19 20:05:57 -0300

    Jogo Par ou Ímpar

---
+++ alpoo/src/main/java/br/ies/aula/alpoo/jogo/Main.java
@@ -0,0 +1,30 @@
+package br.ies.aula.alpoo.jogo;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("all")
+		Scanner leitor = new Scanner(System.in);
+		Integer player1, player2;
+
+		System.out.println("==<>==Jogo Par ou Ímpar!==<>==\n==============================\n");
+		System.out.println("Primeiro jogador, digite um número.");
+		player1 = Integer.valueOf(leitor.nextInt());
+		
+		System.out.println("Segundo jogador, digite um número.");
+		player2 = Integer.valueOf(leitor.nextInt());
+	
+		if((player1 + player2) % 2 == 0) {
+			System.out.println("Par ganhou!!");
+			
+		}else {
+			System.out.println("Ímpar ganhou!!");
+			
+		}
+		
+	}
+	
+}
