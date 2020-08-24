c2a9089a6041a9178b91128e2143d544540383b3
joaop <joaop@DESKTOP-9E9BF0C>
2020-08-24 20:45:04 -0300

    Game 8-Puzzle

--- alpoo/src/test/java/test/br/ies/aula/alpoo/TesteOla.java
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/TesteOla.java
@@ -1,18 +1,41 @@
 package test.br.ies.aula.alpoo;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class TesteOla {
+import br.ies.aula.alpoo.calculadora.Calculadora;
 
+public class TesteCalculadora {
+	private Calculadora calculadora;
+
+	@Before
+	public void setUp() {
+		calculadora = new Calculadora();
+	}
+
+	@Test
+	public void somaComResultado3() {
+		calculadora.soma(2, 1);
+		assertEquals(Float.valueOf(3), calculadora.obterResultado());
+	}
+
+	@Test
+	public void somaComResultado2() {
+		calculadora.soma(1, 1);
+		assertEquals(Float.valueOf(2), calculadora.obterResultado());
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void divisaoPorZero() throws Exception {
+		calculadora.dividir(Integer.valueOf(1),Integer.valueOf(0));
+	}
 	
 	@Test
-	public void ola() {
-		Calculadora Calculadora = new Calculadora();
-		Object calculadora;
-		assertEquals(3,calculadora.obterResultado());
+	public void doisPontoCinco() throws Exception {
+		calculadora.dividir(5, 2);
+		assertEquals(Float.valueOf(2.5f),calculadora.obterResultado());
 	}
-	@Test
+	
 }
-moba
