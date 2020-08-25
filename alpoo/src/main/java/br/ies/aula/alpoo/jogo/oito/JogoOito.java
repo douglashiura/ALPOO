d7be0994d2e62862e0f8e213864d01e9e03e7f45
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-08-24 21:10:38 -0300

    Jogo do Oito

---
+++ alpoo/src/main/java/br/ies/aula/alpoo/jogo/oito/JogoOito.java
@@ -0,0 +1,74 @@
+package br.ies.aula.alpoo.jogo.oito;
+
+public class JogoOito {
+	private static Integer[] tabuleiro = new Integer[9];
+	
+	public JogoOito() {
+		for(Integer i = Integer.valueOf(0); i < Integer.valueOf(9); i++) {
+			tabuleiro[i] = i;
+		}
+	}
+	
+	public void moverPraCima() {
+		Integer local = this.localizacaoNull();
+		if(this.localizacaoNull() > Integer.valueOf(2)) {
+			Integer swap = tabuleiro[this.localizacaoNull() - 3];
+			tabuleiro[this.localizacaoNull() - Integer.valueOf(3)] = 0;
+			tabuleiro[local] = swap;
+		}
+	}
+	
+	public void moverPraBaixo() {
+		Integer local = this.localizacaoNull();
+		if(this.localizacaoNull() < Integer.valueOf(5)) {
+			Integer swap = tabuleiro[this.localizacaoNull() + 3];
+			tabuleiro[this.localizacaoNull() + Integer.valueOf(3)] = 0;
+			tabuleiro[local] = swap;
+		}
+	}
+	
+	public void moverPraDireita() {
+		Integer local = this.localizacaoNull();
+		if(this.localizacaoNull() + 1 < 8) {
+			Integer swap = tabuleiro[this.localizacaoNull() + 1];
+			tabuleiro[this.localizacaoNull() + Integer.valueOf(1)] = 0;
+			tabuleiro[local] = swap;
+		}
+	}
+	
+	public void moverPraEsquerda() {
+		Integer local = this.localizacaoNull();
+		if(this.localizacaoNull() - 1 >= 0) {
+			Integer swap = tabuleiro[this.localizacaoNull() - 1];
+			tabuleiro[this.localizacaoNull() - Integer.valueOf(1)] = 0;
+			tabuleiro[local] = swap;
+		}
+	}
+	
+	public Integer localizacaoNull() {
+		for(Integer i = Integer.valueOf(0); i < Integer.valueOf(9); i++) {
+			if(tabuleiro[i] == Integer.valueOf(0)) return i;
+		}
+		return Integer.valueOf(88);
+	}
+	
+	public void mostrarTabuleiro() {
+		System.out.println(tabuleiro[0] + "" + tabuleiro[1] + "" + tabuleiro[2]);
+		System.out.println(tabuleiro[3] + "" + tabuleiro[4] + "" + tabuleiro[5]);
+		System.out.println(tabuleiro[6] + "" + tabuleiro[7] + "" + tabuleiro[8]);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ alpoo/src/test/java/test/br/ies/aula/alpoo/TesteJogoOito.java
@@ -0,0 +1,42 @@
+package test.br.ies.aula.alpoo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ies.aula.alpoo.jogo.oito.JogoOito;
+
+public class TesteJogoOito {
+
+	JogoOito jogo;
+	
+	@Before
+	public void setup() {
+		jogo = new JogoOito();
+	}
+	
+	@Test
+	public void moverPracima() {
+		jogo.moverPraCima();
+		assertEquals(Integer.valueOf(0), jogo.localizacaoNull());
+	}
+	
+	@Test
+	public void moverPraBaixo() {
+		jogo.moverPraBaixo();
+		assertEquals(Integer.valueOf(3), jogo.localizacaoNull());
+	}
+	
+	@Test
+	public void moverPraDireita() {
+		jogo.moverPraDireita();
+		assertEquals(Integer.valueOf(1), jogo.localizacaoNull());
+	}
+	
+	@Test
+	public void moverPraEsquerda() {
+		jogo.moverPraEsquerda();
+		assertEquals(Integer.valueOf(0), jogo.localizacaoNull());
+	}
+}
