9791b4f8c4b658581e3ecfeede5f19153db4868e
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-09-01 15:08:33 -0300

    Implementação do JComboBox

--- alpoo/src/visao/java/alpoo/MinhaPrimeiraJanela.java
+++ alpoo/src/visao/java/alpoo/MinhaPrimeiraJanela.java
@@ -1,6 +1,7 @@
 package alpoo;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -17,42 +18,68 @@ public class MinhaPrimeiraJanela {
 		
 		JogoImparPar jogo = new JogoImparPar();
 		
+		String[] imparPar = {"Impar", "Par"};
+		
 		JFrame tela = new JFrame();
 		tela.setSize(500, 500);
 		tela.setLayout(null);
 		tela.setTitle("Jogo do Par e Impar");
+		
 		JLabel primeiroCampo = new JLabel("Entrada do primeiro jogador: ");
 		primeiroCampo.setSize(200, 20);
 		primeiroCampo.setLocation(10, 3);
+		
 		JLabel segundoCampo = new JLabel("Entrada do segundo jogador: ");
 		segundoCampo.setSize(200, 20);
 		segundoCampo.setLocation(10, 43);
+		
 		JButton jogarBotao = new JButton("Jogar");
-		
-		
-		
 		jogarBotao.setSize(80, 40);
 		jogarBotao.setLocation(150, 83);
-		JTextField entradaPrimeiro = new JTextField("2");
-		entradaPrimeiro.setSize(200, 20);
-		entradaPrimeiro.setLocation(210, 3);
-		JTextField entradaSegundo = new JTextField("2");
-		entradaSegundo.setSize(200, 20);
-		entradaSegundo.setLocation(210, 43);
+		
+		JTextField entradaPrimeiro = new JTextField(null);
+		entradaPrimeiro.setSize(30, 20);
+		entradaPrimeiro.setLocation(190, 3);
+		entradaPrimeiro.setHorizontalAlignment(JTextField.CENTER);
+		
+		JComboBox<Object> primeiraCaixa = new JComboBox<Object>(imparPar);
+		primeiraCaixa.setSize(200, 20);
+		primeiraCaixa.setLocation(250, 3);
+		
+		JTextField entradaSegundo = new JTextField(null);
+		entradaSegundo.setSize(30, 20);
+		entradaSegundo.setLocation(190, 43);
+		entradaSegundo.setHorizontalAlignment(JTextField.CENTER);
+		
+		JComboBox<Object> segundaCaixa = new JComboBox<Object>(imparPar);
+		segundaCaixa.setSize(200, 20);
+		segundaCaixa.setLocation(250, 43);
 		
 		JLabel resultado = new JLabel();
-		
 		resultado.setSize(200, 20);
-		resultado.setLocation(200, 300);
+		resultado.setLocation(150, 200);
 
 		jogarBotao.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {	
-				Float jogadorUm = Float.parseFloat(entradaPrimeiro.getText());
-				Float jogadorDois = Float.parseFloat(entradaSegundo.getText());
-				resultado.setText(jogo.calculo(jogadorUm, jogadorDois).toString());
+			public void actionPerformed(ActionEvent e) {
+				if(primeiraCaixa.getSelectedItem() == segundaCaixa.getSelectedItem()) {
+					JOptionPane.showMessageDialog(null, "As apostas precisam ser diferente!");
+				}else {
+					Float jogadorUm = Float.parseFloat(entradaPrimeiro.getText());
+					Float jogadorDois = Float.parseFloat(entradaSegundo.getText());
+					
+					if(primeiraCaixa.getSelectedItem().toString().toUpperCase().equals(
+							jogo.calculo(jogadorUm, jogadorDois).toString()) ) {
+						resultado.setText("Primeiro jogador ganhou!");
+					}else {
+						resultado.setText("Segundo jogador ganhou!");						
+					}
+				}
 			}
 		});
+		
+		tela.add(segundaCaixa);
+		tela.add(primeiraCaixa);
 		tela.add(resultado);
 		tela.add(primeiroCampo);
 		tela.add(entradaPrimeiro);
