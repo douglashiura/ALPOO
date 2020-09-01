3e5f29f99ada3e7ede634d2188285567bb6721e1
Vitor Costa <deividevitor565@gmail.com>
2020-08-31 21:18:22 -0300

    fix logic error

--- alpoo/src/main/java/br/ies/aula/alpoo/EvenOrODD/EvenOrODD.java
+++ alpoo/src/main/java/br/ies/aula/alpoo/EvenOrODD/EvenOrODD.java
@@ -3,6 +3,6 @@ package br.ies.aula.alpoo.EvenOrODD;
 public class EvenOrODD {
 	
 	public GameResults playGame(int firstNumber, int secondNumber) {
-		return firstNumber + secondNumber % 2 == 0? GameResults.PAR: GameResults.IMPAR;
+		return (firstNumber + secondNumber) % 2 == 0? GameResults.PAR: GameResults.IMPAR;
 	}
 }
