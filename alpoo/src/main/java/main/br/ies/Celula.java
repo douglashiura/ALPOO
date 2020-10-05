9d796d073c678b89c623ba601915964fbc84b1e3
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-10-05 20:08:32 -0300

    Criação do branch da APS de Victor Rodrigues e Vinicius Amorim

---
+++ alpoo/src/main/java/main/br/ies/Celula.java
@@ -0,0 +1,33 @@
+package main.br.ies;
+
+public class Celula {
+	private Integer numero;
+	private Integer posicao;
+
+	public Celula(Integer numero, Integer posicao) {
+		this.setNumero(numero);
+		this.setPosicao(posicao);
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Integer getPosicao() {
+		return posicao;
+	}
+
+	public void setPosicao(Integer posicao) {
+		this.posicao = posicao;
+	}
+
+	public static void trocarNumero(Celula x, Celula y) {
+		Integer swap = x.getNumero();
+		x.setNumero(y.getNumero());
+		y.setNumero(swap);
+	}
+}
---
+++ alpoo/src/test/java/ies/br/test/TesteDeTrocaDeNumeroDaCelula.java
@@ -0,0 +1,31 @@
+package ies.br.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import main.br.ies.Celula;
+
+public class TesteDeTrocaDeNumeroDaCelula {
+
+	private Celula celula1;
+	private Celula celula2;
+
+	@Test
+	public void trocandoValoresDaPrimeiraCelulaComASegunda() {
+		celula1 = new Celula(Integer.valueOf(5), Integer.valueOf(1));
+		celula2 = new Celula(Integer.valueOf(9), Integer.valueOf(2));
+		Celula.trocarNumero(celula1, celula2);
+
+		assertEquals(9, celula1.getNumero());
+	}
+
+	@Test
+	public void trocandoValoresDaSegundaCelulaComAPrimeira() {
+		celula1 = new Celula(Integer.valueOf(5), Integer.valueOf(1));
+		celula2 = new Celula(Integer.valueOf(9), Integer.valueOf(2));
+		Celula.trocarNumero(celula1, celula2);
+
+		assertEquals(5, celula2.getNumero());
+	}
+}
