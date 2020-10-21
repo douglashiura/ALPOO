d6d53a9e0b5a04b0be623c1f474b84df193c6c58
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-10-21 16:22:06 -0300

    Utilizando Bean

--- alpoo/src/visao/java/alpoo/web/Bean.java
+++ alpoo/src/visao/java/alpoo/web/Bean.java
@@ -7,13 +7,45 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean()
 public class Bean {
 	private String name;
+	private String password;
+	private String autenticado;
 
 	public Bean() {
-		name = "Teste";
+		// name = "Teste";
+		autenticado = "Opa";
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void autenticar() {
+		if (getName().equals(password)) {
+			autenticado = "Tudo certo";
+		} else {
+			autenticado = "Tudo errado";
+		}
+
+	}
+
+	public String getAutenticado() {
+		return autenticado;
+	}
+
+	public void setAutenticado(String autenticado) {
+		this.autenticado = autenticado;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
