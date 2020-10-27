c86722c89db88777c527fce7c51bdc0f0e479444
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-10-27 12:05:45 -0300

    Refatoração do layout Swing e remoção dos botões

--- alpoo/src/visao/java/br/ies/visao/swing/TelaSwingJogoDoOito.java
+++ alpoo/src/visao/java/br/ies/visao/swing/TelaSwingJogoDoOito.java
@@ -2,11 +2,10 @@ package br.ies.visao.swing;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.KeyListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -52,125 +51,69 @@ public class TelaSwingJogoDoOito {
 		frmJogoDoOito.setTitle("Jogo Do Oito");
 		frmJogoDoOito.setBounds(100, 100, 450, 300);
 		frmJogoDoOito.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmJogoDoOito.getContentPane().setLayout(new GridLayout(0, 3, 0, 0));
 
 		JLabel superiorEsquerdo = new JLabel(controle.getTabuleiro().getSuperiorEsquerda().toString());
+		superiorEsquerdo.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		superiorEsquerdo.setHorizontalAlignment(SwingConstants.CENTER);
 		superiorEsquerdo.setBackground(Color.BLACK);
-		superiorEsquerdo.setBounds(117, 28, 34, 34);
 		frmJogoDoOito.getContentPane().add(superiorEsquerdo);
 
 		JLabel superiorCentral = new JLabel(controle.getTabuleiro().getSuperiorCentral().toString());
+		superiorCentral.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		superiorCentral.setHorizontalAlignment(SwingConstants.CENTER);
 		superiorCentral.setBackground(Color.BLACK);
-		superiorCentral.setBounds(190, 28, 34, 34);
 		frmJogoDoOito.getContentPane().add(superiorCentral);
 
 		JLabel superiorDireita = new JLabel(controle.getTabuleiro().getSuperiorDireita().toString());
+		superiorDireita.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		superiorDireita.setHorizontalAlignment(SwingConstants.CENTER);
 		superiorDireita.setBackground(Color.BLACK);
-		superiorDireita.setBounds(262, 28, 34, 34);
 		frmJogoDoOito.getContentPane().add(superiorDireita);
 
 		JLabel centroEsquerda = new JLabel(controle.getTabuleiro().getCentroEsquerda().toString());
+		centroEsquerda.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		centroEsquerda.setHorizontalAlignment(SwingConstants.CENTER);
 		centroEsquerda.setBackground(Color.BLACK);
-		centroEsquerda.setBounds(117, 73, 34, 34);
 		frmJogoDoOito.getContentPane().add(centroEsquerda);
 
 		JLabel centroCentral = new JLabel(controle.getTabuleiro().getCentro().toString());
+		centroCentral.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		centroCentral.setHorizontalAlignment(SwingConstants.CENTER);
 		centroCentral.setBackground(Color.BLACK);
-		centroCentral.setBounds(190, 73, 34, 34);
 		frmJogoDoOito.getContentPane().add(centroCentral);
 
 		JLabel centroDireita = new JLabel(controle.getTabuleiro().getCentroDireita().toString());
+		centroDireita.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		centroDireita.setHorizontalAlignment(SwingConstants.CENTER);
 		centroDireita.setBackground(Color.BLACK);
-		centroDireita.setBounds(262, 73, 34, 34);
 		frmJogoDoOito.getContentPane().add(centroDireita);
 
-		JLabel inferiorCentral = new JLabel(controle.getTabuleiro().getInferiorCentral().toString());
-		inferiorCentral.setHorizontalAlignment(SwingConstants.CENTER);
-		inferiorCentral.setBackground(Color.BLACK);
-		inferiorCentral.setBounds(190, 118, 34, 34);
-		frmJogoDoOito.getContentPane().add(inferiorCentral);
-
 		JLabel inferiorEsquerdo = new JLabel(controle.getTabuleiro().getInferiorEsquerda().toString());
+		inferiorEsquerdo.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		inferiorEsquerdo.setHorizontalAlignment(SwingConstants.CENTER);
 		inferiorEsquerdo.setBackground(Color.BLACK);
-		inferiorEsquerdo.setBounds(117, 118, 34, 34);
 		frmJogoDoOito.getContentPane().add(inferiorEsquerdo);
 
+		JLabel inferiorCentral = new JLabel(controle.getTabuleiro().getInferiorCentral().toString());
+		inferiorCentral.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		inferiorCentral.setHorizontalAlignment(SwingConstants.CENTER);
+		inferiorCentral.setBackground(Color.BLACK);
+		frmJogoDoOito.getContentPane().add(inferiorCentral);
+
 		JLabel inferiorDireita = new JLabel(controle.getTabuleiro().getInferiorDireita().toString());
+		inferiorDireita.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		inferiorDireita.setHorizontalAlignment(SwingConstants.CENTER);
 		inferiorDireita.setBackground(Color.BLACK);
-		inferiorDireita.setBounds(262, 118, 34, 34);
 		frmJogoDoOito.getContentPane().add(inferiorDireita);
-
-		JButton botaoCima = new JButton("Cima");
-		botaoCima.setBounds(156, 190, 107, 23);
-
-		ActionListener cimaPressionado = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				controle.moverPraBaixo();
-				atualizaTodasAsCelulas(superiorEsquerdo, superiorCentral, superiorDireita, centroEsquerda,
-						centroCentral, centroDireita, inferiorCentral, inferiorEsquerdo, inferiorDireita);
-			}
-		};
-
-		botaoCima.addActionListener(cimaPressionado);
-
-		JButton botaoEsquerda = new JButton("Esquerda");
-		botaoEsquerda.setBounds(117, 214, 89, 23);
-
-		ActionListener esquerdaPressionado = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				controle.moverPraDireita();
-				atualizaTodasAsCelulas(superiorEsquerdo, superiorCentral, superiorDireita, centroEsquerda,
-						centroCentral, centroDireita, inferiorCentral, inferiorEsquerdo, inferiorDireita);
-			}
-		};
-
-		botaoEsquerda.addActionListener(esquerdaPressionado);
-		frmJogoDoOito.getContentPane().setLayout(null);
-		frmJogoDoOito.getContentPane().add(botaoEsquerda);
-		frmJogoDoOito.getContentPane().add(botaoCima);
-
-		JButton botaoDireita = new JButton("Direita");
-		botaoDireita.setBounds(213, 214, 89, 23);
-
-		ActionListener direitaPressionado = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				controle.moverPraEsquerda();
-				atualizaTodasAsCelulas(superiorEsquerdo, superiorCentral, superiorDireita, centroEsquerda,
-						centroCentral, centroDireita, inferiorCentral, inferiorEsquerdo, inferiorDireita);
-			}
-		};
-
-		botaoDireita.addActionListener(direitaPressionado);
-		frmJogoDoOito.getContentPane().add(botaoDireita);
-
-		JButton botaoBaixo = new JButton("Baixo");
-		botaoBaixo.setBounds(156, 238, 107, 23);
-
-		ActionListener baixoPressionado = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				controle.moverPraCima();
-				atualizaTodasAsCelulas(superiorEsquerdo, superiorCentral, superiorDireita, centroEsquerda,
-						centroCentral, centroDireita, inferiorCentral, inferiorEsquerdo, inferiorDireita);
-			}
-		};
-
-		botaoBaixo.addActionListener(baixoPressionado);
-		frmJogoDoOito.getContentPane().add(botaoBaixo);
 
 		KeyListener teclado = new Teclado(controle, superiorEsquerdo, superiorCentral, superiorDireita, centroEsquerda,
 				centroCentral, centroDireita, inferiorCentral, inferiorEsquerdo, inferiorDireita, this);
 
-		frmJogoDoOito.addKeyListener(teclado);
-
 		atualizaTodasAsCelulas(superiorEsquerdo, superiorCentral, superiorDireita, centroEsquerda, centroCentral,
 				centroDireita, inferiorCentral, inferiorEsquerdo, inferiorDireita);
+
+		frmJogoDoOito.addKeyListener(teclado);
 	}
 
 	public void atualizaTodasAsCelulas(JLabel superiorEsquerdo, JLabel superiorCentral, JLabel superiorDireita,
