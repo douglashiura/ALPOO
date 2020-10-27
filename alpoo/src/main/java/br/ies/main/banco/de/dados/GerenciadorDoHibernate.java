aa485f6711fece61f07ef7a5efaf3028750d6d0d
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-10-27 18:40:21 -0300

    Hibernate do Tabuleiro

--- alpoo/src/main/java/br/ies/main/banco/de/dados/GerenciadorDoHibernate.java
+++ alpoo/src/main/java/br/ies/main/banco/de/dados/GerenciadorDoHibernate.java
@@ -1,7 +1,43 @@
 package br.ies.main.banco.de.dados;
 
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+
+import br.ies.main.tabuleiro.Tabuleiro;
+
 public class GerenciadorDoHibernate {
+	protected SessionFactory session;
+	private static GerenciadorDoHibernate instancia = new GerenciadorDoHibernate();
+
 	public GerenciadorDoHibernate() {
-		// TODO Auto-generated constructor stub
+		Configuration configuration = new Configuration();
+		Properties properties = new Properties();
+		properties.put(AvailableSettings.DRIVER, "org.postgresql.Driver");
+		properties.put(AvailableSettings.URL, "jdbc:postgresql://localhost:5432/JogoDoOito");
+		properties.put(AvailableSettings.USER, "postgres");
+		properties.put(AvailableSettings.PASS, "Aloka@01");
+		properties.put(AvailableSettings.HBM2DDL_AUTO, "validate");
+		configuration.setProperties(properties);
+		configuration.addAnnotatedClass(Tabuleiro.class);
+		StandardServiceRegistryBuilder serviceRegistry = new StandardServiceRegistryBuilder();
+		StandardServiceRegistry abc = serviceRegistry.applySettings(properties).build();
+		setSession(configuration.buildSessionFactory(abc));
+	}
+
+	public static GerenciadorDoHibernate getInstancia() {
+		return instancia;
+	}
+
+	public SessionFactory getSession() {
+		return session;
+	}
+
+	public void setSession(SessionFactory session) {
+		this.session = session;
 	}
 }
---
+++ alpoo/src/main/java/br/ies/main/banco/de/dados/HibernateDoTabuleiro.java
@@ -0,0 +1,14 @@
+package br.ies.main.banco.de.dados;
+
+import br.ies.main.tabuleiro.Tabuleiro;
+
+public class HibernateDoTabuleiro extends GerenciadorDoHibernate {
+	
+	public HibernateDoTabuleiro() {
+		super();
+	}
+	
+	public void inserirTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
