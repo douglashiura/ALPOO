571f296cecc52961b8c9882a938cf3c85b11fcfb
bernardo <bernardo@bernardo>
2020-10-27 23:32:45 -0300

    SUBINDO BANCO

--- alpoo/src/main/java/db/banco.sql
+++ alpoo/src/main/java/db/banco.sql
@@ -14,3 +14,24 @@ CREATE TABLE tabuleiro(
 
 ALTER TABLE tabuleiro ADD PRIMARY KEY (id);
 
+CREATE OR REPLACE FUNCTION public.move_casas(casa_origem VARCHAR, casa_destino VARCHAR,	id_tabuleiro INTEGER)
+RETURNS BOOLEAN AS $$
+DECLARE
+	valor_casa_origem INTEGER = (SELECT casa_origem FROM tabuleiro WHERE id = id_tabuleiro);
+	valor_casa_destino INTEGER = (SELECT casa_destino FROM tabuleiro WHERE id = id_tabuleiro);
+BEGIN
+	IF valor_casa_origem IS NOT NULL AND valor_casa_destino IS NOT NULL
+		THEN
+			UPDATE tabuleiro
+			SET casa_origem = valor_casa_destino
+			WHERE id = id_tabuleiro;
+			
+			UPDATE tabuleiro
+			SET casa_destino = valor_casa_origem
+			WHERE id = id_tabuleiro;
+			
+			RETURN TRUE;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
