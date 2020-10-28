93466b38fed35d7099e744bb9da65a0cac754791
muguii <murielaaurelio@gmail.com>
2020-10-28 20:33:22 -0300

    Ajuste no metodo multiplicar

--- alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
+++ alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
@@ -1,5 +1,7 @@
 package br.ies.aula.alpoo.calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
 	private Integer resultado;
@@ -9,8 +11,8 @@ public class Calculadora {
 	}
 	
 	public void multiplicar(Integer num1, Integer num2) throws MaxIntegerException {
-		long resultadoLong = num1 * num2;
-		
+		long resultadoLong = (long) num1 * num2;
+		System.out.println(resultadoLong);
 		if (resultadoLong > Integer.MAX_VALUE) {
 			throw new MaxIntegerException("O numero é maior que o limite aceito para um inteiro.");
 		}
@@ -28,6 +30,24 @@ public class Calculadora {
 	
 	public Integer obterResultado() {
 		return resultado;
+	}
+	
+	public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		Scanner scan = new Scanner(System.in);
+		
+		Integer numeroUm = scan.nextInt();
+		Integer numeroDois = scan.nextInt();
+
+		calculadora.dividir(numeroUm, numeroDois);
+		
+		try {
+			calculadora.multiplicar(numeroUm, numeroDois);
+		} catch (MaxIntegerException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		scan.close();
 	}
 
 }
