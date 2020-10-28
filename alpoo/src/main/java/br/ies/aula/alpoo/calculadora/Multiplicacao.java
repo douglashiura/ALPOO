34bfe8d8c5f71939f6d62eb46cb611520eebd52a
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-10-28 20:33:13 -0300

    Exception if arrumado

--- alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Multiplicacao.java
+++ alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Multiplicacao.java
@@ -5,7 +5,9 @@ public class Multiplicacao implements Operação {
 	@Override
 	public Integer calcular(Integer x, Integer y) throws OutOfInteger {
 		long max = Long.parseLong(x.toString()) * Long.parseLong(y.toString());
-		if (max > Integer.MAX_VALUE) throw new OutOfInteger();
+		if (max > Integer.MAX_VALUE) {
+			throw new OutOfInteger();
+		}
 		return x * y;
 
 	}
