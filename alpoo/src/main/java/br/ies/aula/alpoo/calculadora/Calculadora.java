8241271383d0cb04066cf839750e81809f4d4451
muguii <murielaaurelio@gmail.com>
2020-10-28 20:10:33 -0300

    Alterado condição para long

--- alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
+++ alpoo/src/main/java/br/ies/aula/alpoo/calculadora/Calculadora.java
@@ -9,11 +9,13 @@ public class Calculadora {
 	}
 	
 	public void multiplicar(Integer num1, Integer num2) throws MaxIntegerException {
-		if (num1 * num2 > Integer.MAX_VALUE) {
+		long resultadoLong = num1 * num2;
+		
+		if (resultadoLong > Integer.MAX_VALUE) {
 			throw new MaxIntegerException("O numero é maior que o limite aceito para um inteiro.");
 		}
 		
-		resultado = num1 * num2;
+		this.resultado = num1 * num2;
 	}
 	
 	public void dividir(Integer num1, Integer num2) {
